9441cd6842cee3c1182c35da587c16b7f5502c0b
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-03-05 14:20:35 -0500

    #1 Imprimiendo variables y usando php

---
+++ 01_base.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+    
+    <div class="container">
+          <h1>Aprendiendo PHP Basic</h1>
+          
+          <div class="codigo">
+              <?php
+                  echo  "Hola mundo !!";
+                  echo "<br>";
+                  echo "Hola de nuevo";
+              ?>
+          </div>
+    </div>
+    
+  </body>
+</html>
