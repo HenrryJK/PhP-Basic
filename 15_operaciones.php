b5df101d11a0261ea264c262b3609e58750821e7
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-03-08 12:16:09 -0500

    Operaciones en PHP #16

---
+++ 15_operaciones.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900&amp;subset=latin-ext" rel="stylesheet">
+  </head>
+  <body>
+    
+    <div class="container">
+          <h1>Aprendiendo PHP Basic</h1>
+          <div class="codigo">
+              <?php
+                echo 2 + 2;
+                echo "<br/>";
+                echo 2* 3 ;
+                echo "<br/>";
+                echo 4 - 1;
+                echo "<br/>";
+                echo 10 / 5;
+                echo "<br/>";
+                
+                echo 10 * 2 + 6;
+                echo "<br/>";
+                $total= 0;
+                $total +=20;
+                $total +=5;
+                $total +=10;
+                $total /=5;
+                $total *=7;
+                echo "El total es.. : " .$total;
+                echo "<br/>";
+                for ($i=0; $i <= 100 ; $i++) { 
+                if ($i % 2 == 0) {
+                  echo "El numero : " .  $i . " es par.  <br/>";
+                }else{
+                  echo "El numero : " .  $i .  " es impar. <br/";
+                }
+
+                }
+              ?>
+          </div>
+    </div>
+  </body>
+</html>
